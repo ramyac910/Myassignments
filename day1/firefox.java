ef4d0f89fda844491a8905985433ad0d5e39db29
ramyac910 <ramyac10@gmail.com>
2025-12-03 15:53:06 +0530

    Add files via upload
    day1

---
+++ day1/firefox.java
@@ -0,0 +1,23 @@
+package day1;
+
+public class firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	      float browserVersion = 100.2f;
+	      String browserName = "firefox";
+	      Boolean isVisible = true;
+	      int releaseYear = 1998;
+	      char browserLogo = 'f';
+	      
+	      System.out.println("Firefox Browser Version : "+ browserVersion);
+	      System.out.println("Firefox Browser Name : " + browserName);
+	      System.out.println ("Visibility of Browser : " + isVisible);
+	      System.out.println("Firefox Release Year : " + releaseYear);
+	      System.out.println("Firefox Browser logo : " + browserLogo);
+	          
+			
+	}
+
+}
---
+++ day1/welcome.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to Testleaf");
+		
+      
+      
+	}
+
+}
+ 
