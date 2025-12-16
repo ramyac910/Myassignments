c0f71fb99f9aa84bfaacecb18c6f1807706c1845
ramyac910 <ramyac10@gmail.com>
2025-12-16 15:25:24 +0530

    Add files via upload

---
+++ week3/day2/Amazon.java
@@ -0,0 +1,32 @@
+package week3.day2;
+
+public class Amazon extends CanaraBankClass {
+	
+	@Override
+	public void cashOnDelivery() {
+		
+	}
+	
+	@Override
+	public void upiPayments() {
+		
+		
+	}
+	
+	@Override
+	public void cardPayments() {
+		
+	}
+	
+	@Override
+	public void internetBanking() {
+		
+		
+	}
+	public static void main(String[] args) {
+
+		Amazon am = new Amazon();
+		am.recordPaymentDetails();
+	}
+
+}
---
+++ week3/day2/Payments.java
@@ -0,0 +1,11 @@
+package week3.day2;
+
+public interface Payments {
+	// create abstract methods cashondelivery()
+	
+	void cashOnDelivery();
+	void upiPayments();
+	void cardPayments();
+	void internetBanking();
+
+}
