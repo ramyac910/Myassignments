1c5e684891459e9689fac85b41433847d4309064
ramyac910 <ramyac10@gmail.com>
2025-12-16 15:23:59 +0530

    Add files via upload

---
+++ day2/CanaraBankClass.java
@@ -0,0 +1,12 @@
+package week3.day2;
+
+public abstract class CanaraBankClass implements Payments {
+
+	// create recordpaymentdetails()
+	
+	public void recordPaymentDetails() {
+		System.out.println("paid to customer");
+		
+	}
+	
+}
